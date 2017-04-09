3413dc937d72bbffbc3e0d1b2662996cd4409a6a
wangchaomei <365898377@qq.com>
2017-04-09 10:29:58 +0800

    笨方法学python,1/2/3

---
+++ ex1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#是为了加上注释
+print "Hello world!"
+print "Hello Again"
+print "I like typing this"
+print "This is fun"
+print 'Yay! printing'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print 'wcm learn python again'
---
+++ ex3.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+print "I will now count my chickens"
+wangchaomei =1
+
+#算术计算的时候，最好用括号，这样更方便读懂 25+(30/6)
+print "Hens",25 + 30 / 6
+print "Roosters", 100 - 25 * 3 / 4
+
+print "Now I will count the eggs:"
+# 3+2+1-5+(4%2)-(1/4)+6
+print '3 + 2 + 1 - 5 + 4 % 2 -1 / 4.0 + 6=',3 + 2 + 1 - 5 + 4 % 2 -1 / 4.0 + 6
+
+print "Is it True that 3 + 2 < 5 - 7"
+
+print 3 + 2  < 5 - 7
+
+print "What is 3 + 2?" ,3 + 2
+print "What is 5 - 7?",5 -7
+
+print "Oh,that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?",5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is is less or qqual?", 5 <= -2
+
