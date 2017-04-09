76d14e35c84f3bc07df7f0c8cdfee07dc105bb57
no_image <365898377@qq.com>
2017-04-09 11:18:50 +0800

    去掉mymy—_

--- ex5.py
+++ ex5.py
@@ -2,21 +2,21 @@
 '''
 更多的变量和打印
 '''
-my_name = 'WCHM'
-my_age = 30 # not a lie
-my_heigth = 175 # CM
-my_weigth = 65 # KG
-my_eyes = 'Brown'
-my_teeth = 'White'
-my_hair = 'Black'
+name = 'WCHM'
+age = 30 # not a lie
+heigth = 175 # CM
+weigth = 65 # KG
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Black'
 
-print "Let's talk about %s." % my_name
-print "He's %d CM tall."% my_heigth
-print "He's %d KG heavy." %my_weigth
+print "Let's talk about %s." % name
+print "He's %d CM tall."% heigth
+print "He's %d KG heavy." %weigth
 print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee." % my_teeth
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
 
 #this line is tricky ,try to get it excatly right
-print "if I add %d, %d, add %d I get %d." % (my_age, my_heigth, my_weigth, my_age + my_heigth + my_weigth)
+print "if I add %d, %d, add %d I get %d." % (age, heigth, weigth, age + heigth + weigth)
 # %r 就是一个非常有用的，它的含义是"不管什么都打印出来"
