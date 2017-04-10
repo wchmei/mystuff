79e652c0e112d2f97ee597320872e6b473324f7d
no_image <365898377@qq.com>
2017-04-10 22:16:02 +0800

    打印，打印，打印

---
+++ ex9.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# \n 回车, \t  tab
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nJun\nJul\nAug"
+print "Here are the days:", days
+print "Here are the months:", months
+
+print """
+There's something going on here.
+With the three Double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5 or 6.
+"""
