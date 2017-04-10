f92a83d7f1389d27b4c8e86e6611f894ae70c739
no_image <365898377@qq.com>
2017-04-10 22:37:32 +0800

    tab 转义

---
+++ ex10.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+today_cat = "\tI'm tabbed in."
+# I'm tabbed in.
+persian_cat = "I'm split\non a line."
+"""
+I'm split
+on a line.
+"""
+backslash_cat ="I'm \\ a \\ cat."
+""" I'm \ a \ cat."""
+fat_cat ="""
+I'll do a lis:
+\t* Cat food
+|t* Fishies
+\t* Catnip \n\t* Grass
+"""
+
+print today_cat
+print persian_cat
+print backslash_cat
+print fat_cat
