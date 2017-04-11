47bf991c907e4488c0800141d5aabf4a5b2abdbd
wangchaomei <365898377@qq.com>
2017-04-11 08:44:53 +0800

    raw_input 的提示用法。

---
+++ ex12.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+name = raw_input("What's your name? ")
+age = raw_input("How old are you? ")
+heigth = raw_input("How tall are you? ")
+weigth = raw_input("How much do you weigth? ")
+
+print "How are you my friend %s ,So, you're %r old, %r tall and %r heavy." %(
+		name, age, heigth, weigth)
