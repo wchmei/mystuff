0da2d333952904e6cc0042fcd5fe947eea7a436a
wangchaomei <365898377@qq.com>
2017-04-11 08:37:45 +0800

    添加 %r 和 %s 的说明，打印中文得用%s

--- ex11.py
+++ ex11.py
@@ -6,5 +6,5 @@ print "How tall are you?",
 heigth = raw_input()
 print "How much do you weigth?",
 weigth = raw_input()
-
+# 如果输入的是中文，那么就得将 %r 替换为 %s 就可以打印出中文
 print "So, you're %s old,%r tall and %r heavy."% (age, heigth, weigth)
