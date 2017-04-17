0c125131eb3392f2b51ff8b15f4d8c6b3dda916f
wangchaomei <365898377@qq.com>
2017-04-17 17:10:15 +0800

    更多文件操作,简化脚本

--- ex17.py
+++ ex17.py
@@ -7,19 +7,20 @@ script, from_file, to_file = argv
 print "Copying from %s to %s" % (from_file, to_file)
 
 # we could do these two on one line too,how?
-in_file = open(from_file)
-indata = in_file.read()
-
+# in_file = open(from_file)
+# indata = in_file.read()
+indata = open(from_file).read()
 print "The input file is %d  bytes long" % len(indata)
 
 print "Does the output file exist? %r" % exists(to_file)
 print "Ready, hit RETURN to continue,CRTL-C to abort."
 raw_input()
 
-out_file = open(to_file, 'w')
-out_file.write(indata)
-
+# out_file = open(to_file, 'w')
+# out_file.write(indata)
+open(to_file, 'w').write(indata)
 print "Altigth, all done."
 
-out_file.close()
-in_file.close()
+# 使用了 in_file = open(from_file).read() 就可以不用在最后关闭文件
+# out_file.close()
+# in_file.close()
